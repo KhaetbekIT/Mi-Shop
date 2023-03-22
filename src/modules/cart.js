6b7cf981b9a791177d0434182e0b97dd0e91e96f
Khaetbek <hayotbek-web@mail.ru>
2023-03-23 00:44:28 +0500

    #fix #filter Разработал фтльтер в категорий и товара

--- src/modules/cart.js
+++ src/modules/cart.js
@@ -1,8 +1,9 @@
 import { GetAPIFunc, PostDataFunc } from "./api"
-import {BusketFunc} from "./busket"
+import { BusketFunc } from "./busket"
 
 export const CartFunc = () => {
     const container = document.querySelector("#products-container")
+    const inputCatalogSearch = document.querySelector("#catalog-search")
 
     const init = () => {
         const params = window.location.search
@@ -43,6 +44,43 @@ export const CartFunc = () => {
         })
     }
 
+    inputCatalogSearch.addEventListener("input", (e) => {
+        GetAPIFunc(`/products?q=${e.target.value}`).then(data => {
+
+            container.innerHTML = ""
+
+            data.map(item => {
+                const { id, title, productName, price, image } = item
+                container.insertAdjacentHTML("beforeend", `
+                    <div class="col col-12 col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <a href="#!" class="card-link">
+                            <div class="card">
+    
+                                <img src="${image}" class="card-img-top" alt="${title}" title="${title}">
+    
+                                <div class="card-body">
+                                    <span class="mb-2 d-block text-secondary">${productName}</span>
+                                    <h6 class="card-title mb-3">${title}</h6>
+    
+                                    <div class="row">
+                                        <div class="col d-flex align-itemns-center justify-content-between">
+                                            <h4>${price} ₽</h4>
+                                            <button type="button" class="btn btn-outline-dark" data-product-id="${id}">
+    
+                                                <img src="./images/icon/shopping-cart-big.svg" alt="login">
+    
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                `)
+            })
+        })
+    })
+
     container.addEventListener("click", (e) => {
         if (e.target.closest("button")) {
             const id = e.target.closest("button").dataset.productId
@@ -58,7 +96,7 @@ export const CartFunc = () => {
                     headers: {
                         "Content-Type": "application/json"
                     }
-                }).then(()=>{
+                }).then(() => {
                     BusketFunc()
                 })
             })
--- src/modules/categories.js
+++ src/modules/categories.js
@@ -1,10 +1,15 @@
-import{GetAPIFunc} from "./api"
-export const CategoriesFunc = ()=>{
+import { GetAPIFunc } from "./api"
+export const CategoriesFunc = () => {
+
     const container = document.querySelector("#categories-container")
-    
-    GetAPIFunc("/catalog").then(data =>{
-        data.map(item=>{
-            const {id, category, name, image} = item
+    const inputCatalogSearch = document.querySelector("#catalog-search")
+
+    GetAPIFunc("/catalog").then(data => {
+
+        container.innerHTML = ""
+
+        data.map(item => {
+            const { id, category, name, image } = item
             container.insertAdjacentHTML("beforeend", `
             <div class="col col-12 col-md-6 col-lg-4 mb-3">
 
@@ -20,4 +25,28 @@ export const CategoriesFunc = ()=>{
             `)
         })
     })
+
+    inputCatalogSearch.addEventListener("input", (e)=>{
+        GetAPIFunc(`/catalog?q=${e.target.value}`).then(data => {
+
+            container.innerHTML = ""
+    
+            data.map(item => {
+                const { id, category, name, image } = item
+                container.insertAdjacentHTML("beforeend", `
+                <div class="col col-12 col-md-6 col-lg-4 mb-3">
+    
+                    <a href="./catalog.html?id=${id}" class="card-link">
+                        <div class="card">
+                            <img src="${image}" class="card-img-top" alt="phones">
+                            <div class="card-body">
+                                <h5 class="card-title">${name}</h5>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                `)
+            })
+        })
+    })    
 }
